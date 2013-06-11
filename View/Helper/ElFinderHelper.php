2b3f6c81061b31fb78b155bc7fbadc3d17c9b7b8
Paul Gardner <paul@Pauls-MacBook-Pro.local>
2013-06-11 19:25:03 +0100

    include css and js files always

--- View/Helper/ElFinderHelper.php
+++ View/Helper/ElFinderHelper.php
@@ -43,26 +43,24 @@ class ElFinderHelper extends AppHelper {
  * @return void
  */
 	public function beforeRender($viewFile) {
+		$this->Html->css(
+		  array(
+		    'http://code.jquery.com/ui/1.8.18/themes/smoothness/jquery-ui.css', 
+		    '/ElFinder/elfinder/css/elfinder.min'
+		  ),
+		  null,
+		  array('inline' => false)
+		);
+	  $this->Html->script(
+	    array('/ElFinder/elfinder/js/elfinder.min'),
+	    array('inline' => false)
+	  );
+	  
 		if (is_array(Configure::read('Wysiwyg.actions'))) {
 			$this->actions = Hash::merge($this->actions, Configure::read('Wysiwyg.actions'));
 		}
 		$action = Inflector::camelize($this->params['controller']) . '/' . $this->params['action'];
-		if ($action === 'ElFinder/admin_index') {
-  		$this->Html->css(
-  		  array(
-  		    'http://code.jquery.com/ui/1.8.18/themes/smoothness/jquery-ui.css', 
-  		    '/ElFinder/elfinder/css/elfinder.min'
-  		  ),
-  		  null,
-  		  array('inline' => false)
-  		);
-  		$this->Html->script(
-  		  array(
-  		    '/ElFinder/elfinder/js/elfinder.min',
-  		  ),
-  		  array('inline' => false)
-  		);
-		} elseif (Configure::read('Writing.wysiwyg') && isset($this->actions[$action])) {
+		if (Configure::read('Writing.wysiwyg') && isset($this->actions[$action])) {
 		  $this->_CroogoPlugin = new CroogoPlugin();
 		  if ($this->_CroogoPlugin->isActive('ckeditor')) {
 	  	  Configure::write('Js.Wysiwyg.attachmentsPath', Router::url('/ElFinder/elfinder/elfinder-ckeditor.html'));
